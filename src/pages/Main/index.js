5e0c8f2491f7f5486805ecfc7934fcc7ab9cfe29
Mateus Alencar <mtauws@gmail.com>
2020-07-13 16:49:34 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import { Form, SubmitButton, List } from './styles';
+import Container from '../../Components/Container';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    error: false,
+  };
+
+  // carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // salvar os dados no localSotrage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    try {
+      e.preventDefault();
+      this.setState({
+        loading: true,
+      });
+      const { newRepo, repositories } = this.state;
+
+      if (repositories.find(repo => repo.name === newRepo)) {
+        throw 'Repositório duplicado';
+      }
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+        error: false,
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    const { newRepo, loading, repositories, error } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit} error={error}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? ( // se o loading for true mostra o spinner
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              // senão mostra o sinal de mais
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
